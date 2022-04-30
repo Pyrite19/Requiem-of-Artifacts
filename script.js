a11d8c47e525362a823bbc2929ff4ec22785a122
BuildTools <unconfigured@null.spigotmc.org>
2022-04-30 01:41:04 -0500

    Blinking logic complete

--- script.js
+++ script.js
@@ -3,3 +3,7 @@ const eye = document.querySelector('#eye');
 eye.addEventListener('mouseover', (e) => {
     eye.src = "images/eye2.png";
 });
+
+eye.addEventListener('mouseout', (e) => {
+    eye.src = "images/eye1.png";
+});
