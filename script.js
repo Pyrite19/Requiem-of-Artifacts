4674414eda9974af56bbfea0b9a97fe885c8cf9e
BuildTools <unconfigured@null.spigotmc.org>
2022-04-30 23:21:25 -0500

    Eye duplication implemented

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
-const eye = document.querySelector('#eye');
+let counter = 0;
+let duped = 0;
+const eye = document.querySelector(".eye");
+const container = document.querySelector(".container");
 
-eye.addEventListener('mouseover', (e) => {
-    eye.src = "images/eye2.png";
+eye.addEventListener("mouseover", (e) => {
+  eye.src = "images/eye2.png";
 });
 
-eye.addEventListener('mouseout', (e) => {
-    eye.src = "images/eye1.png";
+eye.addEventListener("mouseout", (e) => {
+  eye.src = "images/eye1.png";
+  if (counter == 2 && duped == 0) {
+    const img = document.createElement("img");
+    img.src = "images/eye1.png";
+    img.classList.add("eye");
+    container.appendChild(img);
+    duped = 1;
+  } else {
+    counter += 1;
+    console.log(counter);
+  }
 });
