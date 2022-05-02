c8fc213be34c8991405d97fd8ebc791632bde62b
BuildTools <unconfigured@null.spigotmc.org>
2022-05-01 23:06:01 -0500

    Added some background images to be used, started flexboxes.

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 Site made for a student project
 
 https://www.eye-on.ai/ - temporary eye used for testing
+https://wallpapercave.com/4k-pc-wallpapers - background
+https://www.peakpx.com/en/hd-wallpaper-desktop-nrsfn - background
+https://wallpaperaccess.com/angel-aesthetic-desktop - background
+https://wallpapersafari.com/w/Oe5EIP - background
+https://wallpaperaccess.com/weirdcore - background
+https://wallpapercave.com/w/wp10607016 - homepage background
--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-let counter = 0;
-let duped = 0;
-const eye = document.querySelector(".eye");
-const container = document.querySelector(".container");
+const eye = document.querySelectorAll(".eye");
 
 eye.addEventListener("mouseover", (e) => {
   eye.src = "images/eye2.png";
@@ -9,14 +6,4 @@ eye.addEventListener("mouseover", (e) => {
 
 eye.addEventListener("mouseout", (e) => {
   eye.src = "images/eye1.png";
-  if (counter == 2 && duped == 0) {
-    const img = document.createElement("img");
-    img.src = "images/eye1.png";
-    img.classList.add("eye");
-    container.appendChild(img);
-    duped = 1;
-  } else {
-    counter += 1;
-    console.log(counter);
-  }
 });
