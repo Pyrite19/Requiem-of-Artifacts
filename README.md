97992f32657a25582cfb386be6d4a5134c56a855
BuildTools <unconfigured@null.spigotmc.org>
2022-05-02 20:54:42 -0500

    Added and organized all images, added blinking and dilation to homepage, fixed homepage CSS.

--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 Site made for a student project
 
 https://www.eye-on.ai/ - temporary eye used for testing
-https://wallpapercave.com/4k-pc-wallpapers - background
-https://www.peakpx.com/en/hd-wallpaper-desktop-nrsfn - background
-https://wallpaperaccess.com/angel-aesthetic-desktop - background
-https://wallpapersafari.com/w/Oe5EIP - background
-https://wallpaperaccess.com/weirdcore - background
-https://wallpapercave.com/w/wp10607016 - homepage background
+https://wallpapercave.com/4k-pc-wallpapers - Isolation background
+https://www.peakpx.com/en/hd-wallpaper-desktop-nrsfn - Ballpit background
+https://wallpaperaccess.com/angel-aesthetic-desktop - Chapel background
+https://wallpapersafari.com/w/Oe5EIP - Sunset background
+https://wallpaperaccess.com/weirdcore - Bedroom background
+https://wallpapercave.com/w/wp10607016 - Homepage background
--- script.js
+++ script.js
@@ -1,9 +1,46 @@
-const eye = document.querySelectorAll(".eye");
+const home = document.querySelector(".homeEye");
+const ballpit = document.querySelector(".ballpitEye");
+const bedroom = document.querySelector(".bedroomEye");
+const chapel = document.querySelector(".chapelEye");
+const sunset = document.querySelector(".sunsetEye");
+const isolation = document.querySelector(".isolationEye");
+let dilated = false;
 
-eye.addEventListener("mouseover", (e) => {
-  eye.src = "images/eye2.png";
+eyesArray = new Array(home, ballpit, bedroom, chapel, sunset, isolation).forEach(eye => {
+  eye.addEventListener("mouseover", (e) => {
+    dilated = true
+    eye.src = eye.src.replace("blink", "dilated")
+    eye.src = eye.src.replace("eye", "dilated")
+  });
+  eye.addEventListener("mouseout", (e) => {
+    dilated = false
+    eye.src = eye.src.replace("dilated", "eye")
+  });
+  timedBlink(eye)
 });
 
-eye.addEventListener("mouseout", (e) => {
-  eye.src = "images/eye1.png";
-});
+// eye.addEventListener("mouseover", (e) => {
+//   eye.src = "images/eye2.png";
+// });
+
+// eye.addEventListener("mouseout", (e) => {
+//   eye.src = "images/eye1.png";
+// });
+
+function timedBlink(eye) {
+  if (!dilated) {
+    eye.src = eye.src.replace("blink", "eye")
+    setTimeout(() => {
+      blink(eye)
+    }, (Math.random() * 20000))
+  }
+};
+
+function blink(eye) {
+  if (!dilated) {
+    eye.src = eye.src.replace("eye", "blink")
+    setTimeout(() => {
+      timedBlink(eye)
+    }, (Math.random() * 500))
+  }
+};
